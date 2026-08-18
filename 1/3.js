0471d64dfb6bb72121c18a363ef08225b34b8ea2
Lucky Sah <lk4179869@gmail.com>
2026-08-18 11:22:27 +0530

    array: activity

--- 1/3.js
+++ 1/3.js
@@ -1,5 +1,22 @@
-let students= ["Aman", "Rahul", "Lucky"];
-console.log(students[1]);
-console.log(students[2]);
-console.log(students[0]);
-console.log(students.length)
+// let students= ["Aman", "Rahul", "Lucky"];
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[0]);
+// console.log(students.length)
+
+
+//Activity 1
+let marks= [65, 82, 45, 91, 73];
+let passing_students=0;
+let sum =0;
+for (let i=0; i<marks.length; i++){
+    sum+=marks[i];
+    if(marks[i]>=30){
+        passing_students++;
+    }
+}
+let average= sum/marks.length;
+
+console.log(sum);
+console.log(passing_students);
+console.log("average=", average);
