48e837e158fbb265347c553fc5154223b19e83cc
Lucky Sah <lk4179869@gmail.com>
2026-08-18 11:39:50 +0530

    add: functions

---
+++ 1/4.js
@@ -0,0 +1,21 @@
+//functions
+function sum(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+function mul(a,b){
+    console.log(a*b);
+}
+
+function div(a,b){
+    console.log(a/b);
+}
+
+sum(2,3);
+sub(2,3);
+mul(2,3);
+div(2,3)
