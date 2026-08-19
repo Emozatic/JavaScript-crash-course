9f7a66351201380532b29691552eee25884ed827
Lucky Sah <lk4179869@gmail.com>
2026-08-19 20:16:02 +0530

    add: callback activity

---
+++ 1/20.js
@@ -0,0 +1,21 @@
+// function greet(name){
+//     console.log("HEllo"+name);
+// }
+// function processUser(callback){
+//     callback("Lucky")
+// }
+// processUser(greet);
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// function multiply(a,b){
+//     console.log(a*b);
+// }
+// function calculate(a,b,operation){
+//     operation(a,b)
+// }
+// calculate(2,1,multiply)
+
+
+
