8a2c0b482dfd08c681e817c8664abe5a2a3ad090
Lucky Sah <lk4179869@gmail.com>
2026-08-19 20:23:38 +0530

    add: callback activity 2

--- 1/20.js
+++ 1/20.js
@@ -18,14 +18,34 @@
 // calculate(2,1,multiply)
 
 //activity 2
-function greet(name){
-    console.log("Good Morning" +name);
-}
-function bye(name){
-    console.log("Bye"+name);
-}
-function access(name, operation){
-    operation(name);
-}
-access("Lucky", bye)
+// function greet(name){
+//     console.log("Good Morning" +name);
+// }
+// function bye(name){
+//     console.log("Bye"+name);
+// }
+// function access(name, operation){
+//     operation(name);
+// }
+// access("Lucky", bye)
 
+
+//activity 3
+function square(num){
+    num.forEach((num)=>{
+    console.log(num*num);
+    })
+}
+
+function double(num){
+    console.log(num)
+    num.forEach((num)=>{
+    console.log(num*2);
+    })
+}
+
+function master(num, operation){
+    operation(num);
+}
+let num= [2,3,4,5];
+master(num, double)
