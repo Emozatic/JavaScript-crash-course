25df23c789f3ad771c3bf2ec4f487a693995e352
Lucky Sah <lk4179869@gmail.com>
2026-08-19 18:24:42 +0530

    add: sorting activity

--- 1/17.js
+++ 1/17.js
@@ -8,3 +8,18 @@ nums.sort((a,b)=>{
     return a-b
 })
 console.log(nums)
+
+let marks= [72, 45, 91, 63, 88, 56, 100, 34];
+// marks.sort((a,b)=>{
+//     return a-b;
+// })
+
+
+
+marks.sort((a,b)=>{
+    return b-a;
+})
+console.log(marks)
+console.log("first", marks[0])
+console.log("second", marks[1])
+console.log("third", marks[2])
