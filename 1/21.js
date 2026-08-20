dc32ad5cdae6cd905c32d4073feaf1d51aca5b29
Lucky Sah <lk4179869@gmail.com>
2026-08-20 08:05:52 +0530

    add: HOF practise

--- 1/21.js
+++ 1/21.js
@@ -7,3 +7,18 @@ function multiply(a,b){
     return a*b;
 }
 console.log(calculate(5,4,multiply))
+
+//mymap
+function createMap(num, operation){
+    let result=[];
+    for(let i=0; i<num.length; i++){
+        result.push(operation(num[i]))
+    }
+    return result;
+}
+function mymap(num){
+    return num*2;
+}
+
+let numbers= [1,2,3,4,5];
+console.log(createMap(numbers, mymap))
