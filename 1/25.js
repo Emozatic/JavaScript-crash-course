d52a5be76632936f622197350b3fd58f0c9a8c9c
Lucky Sah <lk4179869@gmail.com>
2026-08-20 20:39:16 +0530

    add: delayed promises

--- 1/25.js
+++ 1/25.js
@@ -14,16 +14,33 @@
 // })
 
 //activity 1
-let promise= new Promise((resolve,reject)=>{
-    let age =20;
-    if(age>=18){
-        resolve("you are elegible");
-    }
-    else{
-        reject("you are not elegible");
-    }
+// let promise= new Promise((resolve,reject)=>{
+//     let age =20;
+//     if(age>=18){
+//         resolve("you are elegible");
+//     }
+//     else{
+//         reject("you are not elegible");
+//     }
+// }).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+//delayed promises
+let promise= new Promise((resolve, reject)=>{
+    let success= false
+    setTimeout(()=>{
+        if(success){
+            resolve("true");
+        }
+        else{
+            reject("false");
+        }
+    },2000);
 }).then((res)=>{
-    console.log(res);
+    console.log(res)
 }).catch((err)=>{
     console.log(err)
 })
