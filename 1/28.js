a1ad3468a72a690bb80c5592447f43bfe132505f
Lucky Sah <lk4179869@gmail.com>
2026-08-21 09:39:55 +0530

    add: promise.all

--- 1/28.js
+++ 1/28.js
@@ -8,20 +8,36 @@ function getData(name, time){
     })
 }
 
+// async function start(){
+//     try{
+//         let user= await getData("user", 3000);
+//         console.log(user);
+
+//         let posts= await getData("posts", 2000);
+//         console.log(posts);
+
+//         let comments= await getData("comments", 1500)
+//         console.log(comments);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+
+// start();
+
+//promise.all done execution at a time
 async function start(){
     try{
-        let user= await getData("user", 3000);
-        console.log(user);
-
-        let posts= await getData("posts", 2000);
-        console.log(posts);
-
-        let comments= await getData("comments", 1500)
-        console.log(comments);
+        let result= await Promise.all([
+            getData("user", 2000),
+            getData("Posts", 3000),
+            getData("comments", 1500)
+        ])
+        console.log(result);
     }
     catch(err){
         console.log(err);
     }
 }
-
-start();
+start()
