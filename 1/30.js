786422d16e5c512bed92e8b4c451d4a3b3dea083
Lucky Sah <lk4179869@gmail.com>
2026-08-21 15:51:04 +0530

    add: activity 2

--- 1/30.js
+++ 1/30.js
@@ -10,4 +10,40 @@ async function getUser(id){
 console.log(data.address?.city)
     
 }
-getUser(5)
+getUser(5);
+
+//sending data
+// async function sendData(){
+//     let response= await fetch("https://jsonplaceholder.typicode.com/users",{
+//         method:"POST",
+//         headers:{
+//             "Content-Type":"application/json"
+//         },
+//         body:JSON.stringify({
+//             name:"Lucky",
+//             email:"lk@gmail.com"
+//         })
+//     });
+//     let data= await response.json();
+//     console.log(data)
+// }
+
+// sendData();
+
+//send data 2nd time
+async function sendData2(){
+    let response= await fetch("https://jsonplaceholder.typicode.com/users",{
+        method: "POST", // Specify the HTTP method
+      headers: {
+        "Content-Type": "application/json" // Inform the server that the payload is JSON
+      },
+      body: JSON.stringify({
+        name:"Rocky",
+        email:"roky@gmail.com"
+      })
+    })
+    let data= await response.json();
+    console.log(data)
+}
+
+sendData2();
