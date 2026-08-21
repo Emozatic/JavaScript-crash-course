a78c204fd63588f3de1ff3465f0253ade093064c
Lucky Sah <lk4179869@gmail.com>
2026-08-21 21:18:58 +0530

    add: destructuring

---
+++ 1/33.js
@@ -0,0 +1,10 @@
+//destructuring
+let product= {
+    title:"laptop",
+    price: 50000,
+    brand: "Dell"
+}
+const {title, price, brand}= product
+console.log(title);
+console.log(price);
+console.log(brand);
