7d0682b358cb680441693760f861bfb5c4167b35
Lucky Sah <lk4179869@gmail.com>
2026-08-21 21:22:21 +0530

    add: array destructuring

--- 1/33.js
+++ 1/33.js
@@ -8,3 +8,11 @@ const {title, price, brand}= product
 console.log(title);
 console.log(price);
 console.log(brand);
+
+//array destructuring
+let arr= [10,20,30,40,50];
+let [first, second, ]= arr;
+console.log(first);
+console.log(third);
+console.log(forth);
+console.log(fifth);
