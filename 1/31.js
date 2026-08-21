b72fe8c8bc8e89a8589d8ddce4ff9dce9200ed53
Lucky Sah <lk4179869@gmail.com>
2026-08-21 16:18:56 +0530

    add: PUT METHOD

--- 1/31.js
+++ 1/31.js
@@ -1,18 +1,42 @@
-async function fullData(){
+// async function fullData(){
+//     try{
+//     let response= await fetch ("https://jsonplaceholder.typicode.com/users",{
+//         method: "POST", // Specify the HTTP method
+//       headers: {
+//         "Content-Type": "application/json" // Inform the server that the payload is JSON
+//       },
+//       body: JSON.stringify({
+//         name:"lucky2",
+//         age:21,
+//         email:"lk123@gmail.com"
+//       })
+//     })
+//     if(!response.ok){
+//         throw new Error (`HTTP Erro, ${response.status}`)
+//     }
+//     let data= await response.json();
+//     console.log(data);
+// }catch(err){
+//     console.log(err)
+// }
+// }
+// fullData()
+
+//PUT method
+async function putMethod(){
     try{
-    let response= await fetch ("https://jsonplaceholder.typicode.com/users",{
-        method: "POST", // Specify the HTTP method
+    let response= await fetch("https://jsonplaceholder.typicode.com/users/5",{
+        method: "PUT", // Specify the HTTP method
       headers: {
         "Content-Type": "application/json" // Inform the server that the payload is JSON
       },
       body: JSON.stringify({
-        name:"lucky2",
-        age:21,
-        email:"lk123@gmail.com"
+        name:"Lucky Sah",
+        email:"lkk@gmail.com"
       })
     })
-    if(!response.ok){
-        throw new Error (`HTTP Erro, ${response.status}`)
+    if(!response.ok)   {
+        throw new Error("something went wrong")
     }
     let data= await response.json();
     console.log(data);
@@ -20,4 +44,4 @@ async function fullData(){
     console.log(err)
 }
 }
-fullData()
+putMethod()
