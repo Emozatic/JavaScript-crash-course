0389fc70f487175ffed407532984a5a45579e16f
Lucky Sah <lk4179869@gmail.com>
2026-08-22 09:02:35 +0530

    add: default parameter

---
+++ 1/35.js
@@ -0,0 +1,12 @@
+//default parameter
+function greet(name="Rocky"){
+    console.log(`Hello ${name}`)
+}
+
+greet()
+
+//activity
+function calculate(price, discount= 0){
+    console.log(price-discount);
+}
+calculate(1000)
