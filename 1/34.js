de6538e4e21e30675be42e65486d4e076ab156a5
Lucky Sah <lk4179869@gmail.com>
2026-08-22 08:59:43 +0530

    add: array with spread operator

--- 1/34.js
+++ 1/34.js
@@ -1,19 +1,19 @@
-//spread operator
-let arr1= [10,20,30];
-let arr2= [40,50];
-let arr3= [...arr1, ...arr2];
-console.log(arr3);
+// //spread operator
+// let arr1= [10,20,30];
+// let arr2= [40,50];
+// let arr3= [...arr1, ...arr2];
+// console.log(arr3);
 
-//object spread operator
-let user= {
-    name:"Lucky",
-    age: 22,
-}
-let user2= {
-    ...user,
-    city:"kalka"
-}
-console.log(user2)
+// //object spread operator
+// let user= {
+//     name:"Lucky",
+//     age: 22,
+// }
+// let user2= {
+//     ...user,
+//     city:"kalka"
+// }
+// console.log(user2)
 
 //activity
 // let product= {
@@ -26,13 +26,19 @@ console.log(user2)
 // }
 // console.log(updatedProduct);
 
-//overwrite in spread operator
-let product2= {
-    productDetails: "monitor",
-    price: 40000,
-}
-let updatedProduct={
-    ...product2,
-    price:3000
-}
-console.log(updatedProduct)
+// //overwrite in spread operator
+// let product2= {
+//     productDetails: "monitor",
+//     price: 40000,
+// }
+// let updatedProduct={
+//     ...product2,
+//     price:3000
+// }
+// console.log(updatedProduct)
+
+//spread in array
+let arr1= [10,20,30];
+let arr2= [40,50];
+let arr3= [...arr1, ...arr2, 60];
+console.log(arr3)
