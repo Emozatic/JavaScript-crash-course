719d1cd83c612baf953c95d48a33afa31e7fa75a
Lucky Sah <lk4179869@gmail.com>
2026-08-22 09:25:32 +0530

    add: counter activity

--- 1/38.js
+++ 1/38.js
@@ -1,13 +1,40 @@
 //closures
-function outer(){
-    let count= 0;
-    let inner= function(){
-        count++;
-        console.log(count)
+// function outer(){
+//     let count= 0;
+//     let inner= function(){
+//         count++;
+//         console.log(count)
+//     }
+//     return inner
+// }
+// let counter= outer();
+// counter();
+// counter();
+// counter();
+
+
+//activity
+function createCounter(){
+    let count=0;
+    return{
+        increament: function(){
+            count= count+1;
+        },
+        descreament: function(){
+            count= count-1;
+        },
+        getCount:function (){
+            return count
+        }
     }
-    return inner
 }
-let counter= outer();
-counter();
-counter();
-counter();
+
+let counter= createCounter();
+counter.increament();
+counter.increament();
+counter.increament();
+
+console.log(counter.getCount());
+
+counter.descreament();
+console.log(counter.getCount())
