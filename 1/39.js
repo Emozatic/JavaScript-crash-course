d897f777c456e3f1aec3911e5ff6b63fba77c401
Lucky Sah <lk4179869@gmail.com>
2026-08-23 10:45:24 +0530

    add: activity 2

--- 1/39.js
+++ 1/39.js
@@ -10,3 +10,17 @@ let user1= new User("Lucky", 21);
 let user2= new User("Rahul", 25);
 console.log(user1);
 console.log(user2);
+
+
+//activity 2
+// class Car{
+//     // constructor(brand, model){
+//     //     this.brand= brand;
+//     //     this.model= model;
+//     // }
+// }
+
+// //let car1 = new Car("Toyota", "Fortuner");
+// let car2= new Car();
+// //console.log(car1);
+// console.log(car2);
