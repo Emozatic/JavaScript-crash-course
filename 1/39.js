9b3cfa32d4c346f625f8590b835a21c150e1d021
Lucky Sah <lk4179869@gmail.com>
2026-08-23 10:41:23 +0530

    add: classes and object

---
+++ 1/39.js
@@ -0,0 +1,12 @@
+//classes and objects
+class User{
+    constructor(name, age){
+        this.name= name;
+        this.age= age;
+    }
+}
+
+let user1= new User("Lucky", 21);
+let user2= new User("Rahul", 25);
+console.log(user1);
+console.log(user2);
